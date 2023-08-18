d69496952d9536721fb6135636b463ec0f8100fc
wox <maganainurreta@gmail.com>
2023-08-18 12:48:31 -0600

    cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,23 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('.name'); /* Estaba mal la referencia, el "." es para clases */
-const $b = document.querySelector('.blog'); /* Estaba mal la referencia, el "." es para clases */
-const $l = document.querySelector('.location'); /* Estaba mal la referencia "." para clases */
+const n = document.querySelector('.name'); /* Estaba mal la referencia, el "." es para clases */
+const b = document.querySelector('.blog'); /* Estaba mal la referencia, el "." es para clases */
+const l = document.querySelector('.location'); /* Estaba mal la referencia "." para clases */
 
-async function displayUser(username,n,b,l) { /* Faltaban los parámetros n, b, l y async */
-  $n.textContent = 'cargando...';
+async function displayUser(usersEndpoint,username) { /* Faltaban los parámetros n, b, l y async */
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`); /* Faltaba await en vez de async */
   const data = await response.json() /* Faltaba convertir la respuesta a un objeto */
   console.log(data);
-  $n.textContent = `${data.name}`; /* No tenía backticks */
-  $b.textContent = `${data.blog}`; /* No tenía backticks */
-  $l.textContent = `${data.location}`; /* No tenía backticks */
+  n.textContent = `${data.name}`; /* No tenía backticks */
+  b.textContent = `${data.blog}`; /* No tenía backticks */
+  l.textContent = `${data.location}`; /* No tenía backticks */
 }
 
-function handleError(n,err) { /* Faltaban el parámetro n */
+function handleError(n, err) { /* Faltaba el parámetro n */
   console.log('OH NO!');
   console.log(err);
   n.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski')
